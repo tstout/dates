6bf3b76faa311decb5f534c59977b7b9de644a11
Todd Stout <todd.tstout@gmail.com>
2022-02-13 11:19:27 -0600

    add basic calendar fns

--- src/date_util/core.clj
+++ src/date_util/core.clj
@@ -1,4 +1,44 @@
 (ns date-util.core
-  (:require [clojure.tools.cli :refer [parse-opts]]))
+  (:require [clojure.instant :refer [read-instant-date]])
+  (:import [java.time LocalDate ZoneId]))
 
-(defn -main [& args] (println "hello world"))
+(defn parse-date [date-str]
+  (-> date-str
+      read-instant-date
+      .toInstant
+      (.atZone (ZoneId/systemDefault))
+      .toLocalDate))
+
+(defn now []
+  (LocalDate/now))
+
+(defn plus-days [^LocalDate d days]
+  (.plusDays d days))
+
+(defn date [y m d]
+  (LocalDate/of y m d))
+
+(defn add-days [date-str days]
+  (-> date-str
+      parse-date
+      (plus-days days)))
+
+(defn plus-60 [date-str]
+  (add-days date-str 60))
+
+(defn plus-120 [date-str]
+  (add-days date-str 120))
+
+
+(comment
+  *e
+  (parse-date "2017-08-23")
+
+  (bean (parse-date "2017-08-23"))
+
+  (plus-60 "2022-10-20")
+
+  (plus-120 "2022-10-20")
+
+  ;;
+  )
